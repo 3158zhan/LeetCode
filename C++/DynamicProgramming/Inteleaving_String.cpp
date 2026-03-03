bf94abb9d36c10d8ec44fa4b96ae4de683a4dadf
18388 <18388@MULTI>
2026-03-03 23:00:40 +0800

    更新Interleaving String.cpp中的两种动态规划方法
     Author:    詹相银 <18388035791@163.com>

--- C++/DynamicProgramming/Inteleaving_String.cpp
+++ C++/DynamicProgramming/Inteleaving_String.cpp
@@ -52,3 +52,58 @@ class Solution{
 //LeetCode, Interleaving String
 //二维动态规划，时间复杂度O(n^2)，空间复杂度O(n^2)
 
+class Solution{
+    public:
+    bool isInterleave(const string& s1, const string& s2,const string& s3){
+        if(s3.length() != s1.length() + s2.length())
+        return false;
+
+        vector<vector<bool>> f(s1.length() + 1,
+    vector<bool>(s2.length() + 1,ture));
+
+    for(size_t i = 1; i <= s1.length(); ++i)
+        f[i][0] = f[i -1] && s2[i - 1] == s3[i -1];
+
+    for(size_t i = 1; i <= s2.length(); ++i)
+        f[0][i] = f[0][i - 1] && s2[i - 1] == s3[i - 1];
+
+    for(size_t i = 1; i <= s1.length(); ++i)
+        for(size_t j = 1; j <= s2.length(); ++j)
+            f[i][j] = (s1[i - 1] == s3[i + j - 1] && f[i - 1][j])
+                    || (s2[j - 1] == s3[i + j - 1] && f[i][j - 1]);
+
+                    return f[s1.length()][s2.length()];
+    }
+};
+
+//动态规划+滚动数组
+
+//LeetCode, Interleaving String
+//二维动规+滚动数组，时间复杂度O(n^2)，空间复杂度O(n)
+
+class Solution{
+    public:
+    bool isInterleave(const string& s1, const string& s2, const string& s3){
+        if(s1.length() + s2.length() != s3.length())
+            return false;
+
+            if(s1.length() < s2.length())
+                return isInterleave(s2, s1, s3);
+
+                vector<bool> f(s2.length() + 1, ture);
+
+                for(size_t i = 1; i <= s2.length(); ++i)
+                    f[i] = s2[i - 1] == s3[i - 1] && f[i - 1];
+
+                for(size_t i = 1; i <= s1.length(); ++i ){
+                    f[0] = s1[i -1] == s3[i - 1] && f[0];
+
+                    for(size_t j = 1; j <= s2.length(); ++j)
+                        f[j] = (s1[i - 1] == s3[i + j - 1] && f[j])
+                                ||(s2[j - 1] == s3[i + j - 1] && f[j - 1]);
+                }
+
+                return f[s2.length()];
+    }
+    
+};
