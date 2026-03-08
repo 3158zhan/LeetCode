e03e061da3a6fd6407acc13ddb07b994325a51ed
詹相银 <18388035791@163.com>
2026-03-09 01:19:29 +0800

    更新Binary tree level order traversal II的第二种迭代版解法，空间复杂度降低为O(1)

--- C++/Tree/Binary_Tree_Zigzag_Level_Order_Traversal.cpp
+++ C++/Tree/Binary_Tree_Zigzag_Level_Order_Traversal.cpp
@@ -62,3 +62,33 @@ public:
     
 };
 
+//迭代版
+//LeetCode, Binary Tree Level Order Traversal II
+//迭代版，时间复杂度O(n)，空间复杂度O(1)
+
+class Solution{
+public:
+    vector<vector<int>> levelOrder(treeNode *root) {
+        vector<vector<int>> result;
+        if(root == nullptr) return result;
+
+        queue<TreeNode *> current, next;
+        vector<int> level;  //elments in level level
+
+        current.push(root);
+        while(!current.empty()) {
+            while(!current.empty()){
+                TreeNode* node = current.front();
+                current.pop();
+                level.push_back(node->val);
+                if(node->left != nullptr) next.push(node->left);
+                if(node->right != nullptr) next.push(node->right);
+            }
+            result.push_back(level);
+            level.clear();
+            swap(next, current);
+        }
+        reverse(result.begin(), result.end()); //比上题多这一行
+        return result;
+    }
+};
