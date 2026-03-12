a4e38e168cb4edbadb064bc12b2f2d3ca6a7e65f
詹相银 <18388035791@163.com>
2026-03-13 02:11:47 +0800

    添加了Flatten Binary Tree to Linked List的两种递归题解

---
+++ C++/Tree/Flatten_Binary_Tree_to_Linked_List.cpp
@@ -0,0 +1,96 @@
+//2026,xiangyin zhan
+/*****************************************************************
+
+LeetCode 上二叉树的节点定义如下：
+//树的节点
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+}
+
+//LeetCode, Flatten Binary Tree to Linked List 
+//描述
+
+    Given a binary tree, flatten it to a linked list in-place.
+
+    For example, Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+
+
+    The flattened tree should look like:
+
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+
+//分析
+
+    无
+
+
+******************************************************************/
+
+//代码
+
+
+//LeetCode, Flatten Binary Tree to Linked List
+//递归版1，时间复杂度O(n)，空间复杂度O(logn)
+
+class Solution{
+public:
+    void flatten(TreeNode *root) {
+        if (root == nullptr) return;  // 终止条件
+
+        flatten(root->left);
+        flatten(root->right);
+
+        if (nullptr == root->left) return;
+
+        // 三方合并，将左子树所形成的链表插入到root和root->right之间
+        TreeNode *p = root->left;
+        while(p->right) p = p->right; //寻找左链表最后一个节点
+        p->right = root->right;
+        root->right = root->left;
+        root->left = nullptr;
+    }
+};
+
+
+//LeetCode, Flatten Binary Tree to Linked List
+//递归版2，时间复杂度O(n)，空间复杂度O(logn)
+
+class Solution{
+public:
+    void flatten(TreeNode *root) {
+        flatten(root, NULL);
+    }
+private:
+    // 把root所代表树变成链表后，tail跟在该链表后面
+    TreeNode *flatten(TreeNode *root, TreeNode *tail) {
+        if (NULL == root) return tail;
+
+        root->right = flatten(root->left, flatten(root->right, tail));
+        root->left = NULL;
+        return root;
+    }
+};
+
+//LeetCode, Flatten Binary Tree to Linked List
+//迭代版，时间复杂度O(n)，空间复杂度O(logn)
+
+
