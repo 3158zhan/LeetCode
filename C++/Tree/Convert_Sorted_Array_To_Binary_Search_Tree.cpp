dc7ae64e33bdff32daa3acd7a1483716b45d33d5
詹相银 <18388035791@163.com>
2026-03-18 23:18:10 +0800

    添加了Convert Sorted Array to Binary Search Tree分治法的题解

---
+++ C++/Tree/Convert_Sorted_Array_To_Binary_Search_Tree.cpp
@@ -0,0 +1,56 @@
+//2026,xiangyin zhan
+/*****************************************************************
+
+LeetCode 上二叉树的节点定义如下：
+//树的节点
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+}
+
+//LeetCode, Validate Binary Search Tree
+//描述
+
+    Given an array where elements are sorted in ascending order, 
+convert it to a height balanced BST.
+
+//分析
+
+    二分法
+
+******************************************************************/
+
+//代码
+
+
+//LeetCode, Convert Sorted Array to Binary Search Tree
+//分治法，时间复杂度O(n)，空间复杂度O(logn)
+
+class Solution{
+public:
+    TreeNode* SortedArrayToBST(vector<int>& num) {
+        return SortedArrayToBST(num.begin(), num.end());
+    }
+
+    template<typename RandomAccessIterator>
+    TreeNode* SortedArrayToBST(RandomAccessIterator first,
+        RandomAccessIterator last) {
+            const auto length = distance(first, last);
+
+            if(length <= 0) return nullptr; //终止条件
+
+            //三方合并
+
+            auto mid = first + length / 2;
+            TreeNode* root = new TreeNode (*mid);
+            root->left = SortedArrayToBST(first, mid);
+            root->right = SortedArrayToBST(mid + 1, last);
+
+            return root;
+        }
+
+
+};
+
