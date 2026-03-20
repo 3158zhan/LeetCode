f66af0717031a0bc66b60781b3a1728baf27a887
詹相银 <18388035791@163.com>
2026-03-21 00:47:40 +0800

    添加了Sum Root to Leaf Numbers的题解 这暂时是“Tree”下最后一道例题题解。

---
+++ C++/Tree/Sum_Root_To_Leaf_Numbers.cpp
@@ -0,0 +1,57 @@
+//2026,xiangyin zhan
+/*****************************************************************
+
+LeetCode 上二叉树的节点定义如下：
+//树的节点
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+}
+
+//LeetCode, Sum Root to Leaf Numbers
+//描述
+
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+For example,
+    1
+   / \
+  2   3
+    The root-to-leaf path 1->2 represents the number 12. The root-to-leaf path 1->3 represents the number
+13.
+    Return the sum = 12 + 13 = 25.
+
+//分析
+
+    无
+
+
+******************************************************************/
+
+//代码
+
+
+//LeetCode, Sum Root to Leaf Numbers
+//时间复杂度O(n)，空间复杂度O(logn)
+
+class Solution {
+public:
+    int sumNumbers(TreeNode *root) {
+        return dfs(root, 0);
+    }
+private:
+    int dfs(TreeNode *root, int sum) {
+        if (root == nullptr) return 0;
+        if (root->left == nullptr && root->right == nullptr)
+            return sum * 10 + root->val;
+
+        return dfs(root->left, sum * 10 + root->val) +
+                dfs(root->right, sum * 10 + root->val);
+    }
+};
