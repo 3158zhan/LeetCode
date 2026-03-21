a3922422cc7053fccc81d0dbf56d02407badb8b8
詹相银 <18388035791@163.com>
2026-03-22 01:59:50 +0800

    更新了Longest Valid Parenthese的一种新解法

--- C++/StackAndQueue/Longest_Valid_Parentheses.cpp
+++ C++/StackAndQueue/Longest_Valid_Parentheses.cpp
@@ -55,3 +55,27 @@ public:
   }
      
 };
+
+//Dynamic Programming, One Pass
+//LeetCode, Longest Valid Parenthese
+//时间复杂度O(n)，空间复杂度O(n)
+
+class Solution {
+public:
+    int longestValidParentheses(const string& s){
+        vector<int> f(s.size(), 0);
+        int ret = 0;
+        for(int i = s.size() - 2; i >= 0; --i){
+            int match = i + f[i + 1] + 1;
+            // case: "((...))"
+            if(s[i] == '(' && match < s.size() && s[match] == ')') {
+                f[i] = f[i + 1] + 2;
+                //if a valid sequence exist afterwards "((...))()"
+                if(match + 1 < s.size()) f[i] += f[match + 1];
+            }
+            ret = max(ret, f[i])
+        }
+        return ret;
+    }
+     
+};
