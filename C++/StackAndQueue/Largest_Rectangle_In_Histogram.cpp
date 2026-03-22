44fbac1ef423a8f55d87754f2b869972ee1cff69
詹相银 <18388035791@163.com>
2026-03-23 02:00:56 +0800

    更新了上次没有解答的Largest Rectangle in Histogram的题解

--- C++/StackAndQueue/Largest_Rectangle_In_Histogram.cpp
+++ C++/StackAndQueue/Largest_Rectangle_In_Histogram.cpp
@@ -43,3 +43,23 @@ return 10.
 //LeetCode, Largest Rectangle in Histogram
 //时间复杂度O(n)，空间复杂度O(n)
 
+class Solution{
+    public:
+    int largestRectangleArea(Vector<int> &height){
+        stack<int> s;
+        heighr.push_back(0);
+        int result = 0;
+        for(int i = 0; i < height.size(); ){
+            if(s.empty() || height[i] > height[s,top()])
+                s.push(i++);
+            else{
+                int tmp = s.top();
+                s.pop();
+                result = max(result,
+                    height[tmp] * (s.empty() ? i : i - s,top() - 1));
+            }
+        }
+    return result;
+    }
+
+};
