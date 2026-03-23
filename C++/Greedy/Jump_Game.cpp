e0634907acc273eef36f3f5bfa278c90758ffd5a
詹相银 <18388035791@163.com>
2026-03-23 21:58:31 +0800

    更新了Jump Game的第三种思路

--- C++/Greedy/Jump_Game.cpp
+++ C++/Greedy/Jump_Game.cpp
@@ -68,3 +68,18 @@ public:
     }
 
 };
+
+
+//LeetCode, Jump Game
+//思路3,时间复杂度O(n)，空间复杂度O(n)
+
+class Solution {
+public:
+    bool canJump(const vector<int>& nums){
+        vector<int> f(nums.size(); i++){
+            f[i] = max(f[i - 1], nums[i - 1]) - 1;
+            if(f[i] < 0) return false; 
+        }
+        return f[nums.size() - 1] >= 0;
+    }
+};
