b320338f1c1cdb11f27fb9ea0f36f2022c3201a5
詹相银 <18388035791@163.com>
2026-04-09 01:53:33 +0800

    更新了4Sum的第二种题解map做缓存

--- C++/LinearList/4Sum.cpp
+++ C++/LinearList/4Sum.cpp
@@ -66,3 +66,47 @@ public:
     }
 };
 
+//map做缓存
+// LeetCode, 4Sum
+// 用一个hashmap先缓存两个数的和
+// 时间复杂度，平均O(n^2)，最坏O(n^4)，空间复杂度O(n^2)
+class Solution {
+public:
+    vector<vector<int> > fourSum(vector<int> &nums, int target) {
+        vector<vector<int>> result;
+        if (nums.size() < 4) return result;
+        sort(nums.begin(), nums.end());
+
+        unordered_map<int, vector<pair<int, int> > > cache;
+        for (size_t a = 0; a < nums.size(); ++a) {
+            for (size_t b = a + 1; b < nums.size(); ++b) {
+                cache[nums[a] + nums[b]].push_back(pair<int, int>(a, b));
+            }
+        }
+
+        for (int c = 0; c < nums.size(); ++c) {
+            for (size_t d = c + 1; d < nums.size(); ++d) {
+                const int key = target - nums[c] - nums[d];
+                if (cache.find(key) == cache.end()) continue;
+
+                const auto& vec = cache[key];
+                for (size_t k = 0; k < vec.size(); ++k) {
+                    if (c <= vec[k].second)
+                        continue; // 有重叠
+
+                    result.push_back( { nums[vec[k].first],
+                            nums[vec[k].second], nums[c], nums[d] });
+                }
+            }
+        }
+        sort(result.begin(), result.end());
+        result.erase(unique(result.begin(), result.end()), result.end());
+        return result;
+    }
+};
+
+//multimap
+// LeetCode, 4Sum
+// 用一个 hashmap 先缓存两个数的和
+// 时间复杂度O(n^2)，空间复杂度O(n^2)
+
