29f39734d3d4cfce6b4a0f8754a08c15c8ea5ae8
詹相银 <18388035791@163.com>
2026-04-09 01:49:44 +0800

    添加了4Sum 左右夹逼的题解

---
+++ C++/LinearList/4Sum.cpp
@@ -0,0 +1,68 @@
+//2026,xiangyin zhan
+/*****************************************************************
+
+//LeetCode ,线性表
+//这类题目考察线性表的操作，例如，数组，单链表，双向链表等。
+
+
+//LeetCode, 4 Sum
+//描述
+
+    Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+Find all unique quadruplets in the array which gives the sum of target.
+    Note:
+    • Elements in a quadruplet (a, b, c, d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+    • The solution set must not contain duplicate quadruplets.
+    For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+    A solution set is:
+        (-1, 0, 0, 1)
+        (-2, -1, 1, 2)
+        (-2, 0, 0, 2)
+
+//分析
+
+    先排序，然后左右夹逼，复杂度 O(n^3)，会超时。
+    可以用一个hashmap先缓存两个数的和，最终复杂度O(n^3)。这个策略也适用于 3Sum 。
+
+
+******************************************************************/
+
+
+
+//代码
+
+//左右夹逼
+// 先排序，然后左右夹逼，注意跳过重复的数，时间复杂度O(n^2)，空间复杂度O(1)
+// LeetCode, 4Sum
+// 先排序，然后左右夹逼，时间复杂度O(n^3)，空间复杂度O(1)
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        if (nums.size() < 4) return result;
+        sort(nums.begin(), nums.end());
+
+        auto last = nums.end();
+        for (auto a = nums.begin(); a < prev(last, 3); ++a) {
+            for (auto b = next(a); b < prev(last, 2); ++b) {
+                auto c = next(b);
+                auto d = prev(last);
+                while (c < d) {
+                    if (*a + *b + *c + *d < target) {
+                        ++c;
+                    } else if (*a + *b + *c + *d > target) {
+                        --d;
+                    } else {
+                        result.push_back({ *a, *b, *c, *d });
+                        ++c;
+                        --d;
+                    }
+                }
+            }
+        }
+        sort(result.begin(), result.end());
+        result.erase(unique(result.begin(), result.end()), result.end());
+        return result;
+    }
+};
+
